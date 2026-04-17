1a0bafa5ab343534b7dfb553ae065cc4b689854b
Elisa Rodríguez <elisaholguin21@gmail.com>
2026-04-17 12:25:56 -0500

    Prueba de cambios

--- src/app/pages/cart/cart.component.spec.ts
+++ src/app/pages/cart/cart.component.spec.ts
@@ -1,39 +1,21 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { CartComponent } from "./cart.component";
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CartComponent } from './cart.component';
 
+describe('CartComponent', () => {
+  let component: CartComponent;
+  let fixture: ComponentFixture<CartComponent>;
 
-describe('cart component',()=>{
-let component: CartComponent;
-let fixture: ComponentFixture<CartComponent>;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CartComponent],
+    }).compileComponents();
 
-// beforeEach(()=>{
-//     TestBed.configureTestingModule({
-//         imports:[
+    fixture = TestBed.createComponent(CartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//         ],
-//         declarations:[
-            
-//         ],
-//         providers:[
-
-//         ]
-//     })
-// })
-
-
-// alllooooooo
-
-beforeEach(()=>{
-    TestBed.configureTestingModule({
-        imports:[
-
-        ],
-        declarations:[
-            
-        ],
-        providers:[
-            
-        ]
-    })
-})
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
 });
