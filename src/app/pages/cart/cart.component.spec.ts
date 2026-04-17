113ee4124ce373969437be1abe0ef317501136b2
Elisa Rodríguez <elisaholguin21@gmail.com>
2026-04-17 11:25:35 -0500

    Primer subida del proyecto

---
+++ src/app/pages/cart/cart.component.spec.ts
@@ -0,0 +1,8 @@
+import { ComponentFixture } from "@angular/core/testing";
+import { CartComponent } from "./cart.component";
+
+
+describe('cart component',()=>{
+let component: CartComponent;
+let fixture: ComponentFixture<CartComponent>;
+});git 
