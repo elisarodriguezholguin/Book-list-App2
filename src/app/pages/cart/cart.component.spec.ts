247b6df0d5666942cec067f2c24602e012986c41
ElisaRodriguez95 <44856961+elisarodriguezholguin@users.noreply.github.com>
2026-04-19 17:45:04 -0500

    Agregado arreglo listBook y lógica de tests

--- src/app/pages/cart/cart.component.spec.ts
+++ src/app/pages/cart/cart.component.spec.ts
@@ -3,6 +3,34 @@ import { CartComponent } from './cart.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { BookService } from 'src/app/services/book.service';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { Book } from 'src/app/models/book.model';
+
+
+
+const listBook: Book[] = [
+{
+  name:'',
+  author:'',
+  isbn:'',
+  price:15,
+  amount:2
+},
+{
+  name:'',
+  author:'',
+  isbn:'',
+  price:20,
+  amount:1
+},
+{
+  name:'',
+  author:'',
+  isbn:'',
+  price:8,
+  amount:1
+}
+
+]
 describe('CartComponent', () => {
   let component: CartComponent;
   let fixture: ComponentFixture<CartComponent>;
@@ -28,6 +56,21 @@ describe('CartComponent', () => {
        fixture.detectChanges();
       });
       it('should create',()=>{
-        expect(component).toBeFalse();
+        expect(component).toBeTruthy();
       });
+      
+      // public getTotalPrice(listCartBook: Book[]): number {
+      //    let totalPrice = 0;
+       //   listCartBook.forEach((book: Book) => {
+       //     totalPrice += book.amount! * book.price!;
+       //   });
+       //   return totalPrice;
+       // }
+      
+         it ('getTotalPrice returns an amount' ,()=>{
+            const totalPrice =component.getTotalPrice(listBook);
+            expect(totalPrice).toBeGreaterThan(0);
+            expect(totalPrice).toBeNull();
+            
+ } )
 });
