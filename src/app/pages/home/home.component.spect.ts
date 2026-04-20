e8d90229af39963468c40a2fe3953e06c69fec15
Elisa Rodríguez <elisaholguin21@gmail.com>
2026-04-20 13:15:54 -0500

    Fix: correccion de imports y beforeEach en Home

--- src/app/pages/home/home.component.spect.ts
+++ src/app/pages/home/home.component.spect.ts
@@ -2,24 +2,30 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { HomeComponent } from "./home.component"
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BookService } from "src/app/services/book.service";
 
 
 describe('Home component',()=>{
     let component:HomeComponent;
     let fixture:ComponentFixture<HomeComponent>
 })
-foreEach(() =>{
+beforeEach(() =>{
     TestBed.configureTestingModule({
     imports:[
         HttpClientTestingModule
     ],
     declarations:[
-
+       HomeComponent
     ],
     providers:[
-
+       BookService
     ],
     schemas:[CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     
    }).compileComponents()
    })
+   beforeEach(() => {
+     fixture  = TestBed.createComponent(HomeComponent);
+     component = fixture.
+
+     })
